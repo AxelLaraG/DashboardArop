2e3d4da80d5ea65b524d3a73631667704137f177
AxelLaraG <axmadlar@gmail.com>
2025-12-05 01:33:50 -0600

    UI update, top-bar get icons now

--- src/app/core/models/icons.ts
+++ src/app/core/models/icons.ts
@@ -2,7 +2,7 @@ export const icons = {
   dashboard: `
     <animated-icons
       src="https://animatedicons.co/get-icon?name=dashboard&style=minimalistic&token=fc5c52bc-b9f4-4fb9-981d-70e44b853063"
-      trigger="hover"
+      trigger="loop-on-hover"
       attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
       height="100%"
       width="100%"
@@ -11,7 +11,7 @@ export const icons = {
   users: `
     <animated-icons
       src="https://animatedicons.co/get-icon?name=Staff&style=minimalistic&token=ae89d06d-f628-4b1b-a043-ad9291637752"
-      trigger="hover"
+      trigger="loop-on-hover"
       attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
       height="100%"
       width="100%"
@@ -20,7 +20,7 @@ export const icons = {
   store: `
     <animated-icons
       src="https://animatedicons.co/get-icon?name=Marketplace&style=minimalistic&token=64371c87-8525-4c8a-a147-399f1a8b72f5"
-      trigger="hover"
+      trigger="loop-on-hover"
       attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
       height="100%"
       width="100%"
@@ -29,7 +29,7 @@ export const icons = {
   pedidos: `
     <animated-icons
       src="https://animatedicons.co/get-icon?name=Product&style=minimalistic&token=64371c87-8525-4c8a-a147-399f1a8b72f5"
-      trigger="hover"
+      trigger="loop-on-hover"
       attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
       height="100%"
       width="100%"
@@ -38,8 +38,26 @@ export const icons = {
   productos: `
     <animated-icons
       src="https://animatedicons.co/get-icon?name=Warehouse&style=minimalistic&token=64371c87-8525-4c8a-a147-399f1a8b72f5"
-      trigger="hover"
+      trigger="loop-on-hover"
       attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
+      height="100%"
+      width="100%"
+    ></animated-icons>
+  `,
+  lightMode: `
+    <animated-icons
+      src="https://animatedicons.co/get-icon?name=Light%20Mode&style=minimalistic&token=5f4d2675-67d4-4c5e-9139-8226e36723ae"
+      trigger="click"
+      attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#536DFE","background":"#FFFFFF"}}'
+      height="100%"
+      width="100%"
+    ></animated-icons>
+  `,
+  warning: `
+    <animated-icons
+      src="https://animatedicons.co/get-icon?name=Alert&style=minimalistic&token=4ff92fe8-3f2e-471b-beff-2c28789b1813"
+      trigger="loop"
+      attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","group-2":"#000000FF","background":"#FF6969FF"}}'
       height="100%"
       width="100%"
     ></animated-icons>
